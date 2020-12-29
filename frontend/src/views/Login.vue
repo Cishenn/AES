2e7abb52d935a7f9c5d6c07a2486bbf00efa0571
zzx <1678599489@qq.com>
2020-12-29 21:11:42 +0800

    Finshed login and regist page design. Ref #16

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,5 +1,144 @@
 <template>
-  <div class="login">
-    <h1>This is an login page</h1>
+  <div class="box">
+    <!-- <img src="../assets/back-grond.jpg" alt=""> -->
+    <img src="" alt="">
+    <div class="login-box">
+      <div class="login-title"><h1 class="login-title">Login</h1></div>
+      <div class="form">
+        <div class="line">
+          <i class="el-icon-school"></i>
+          <el-select class="select-box" clearable @change="changeselect" v-model="value" placeholder="Please select your identity.">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+        <div class="line">
+          <i class="el-icon-user"></i>
+          <el-input v-model="username" class="item" type="text" placeholder="Please enter your username." />
+        </div>
+        <div class="line">
+          <i class="el-icon-key"></i>
+          <el-input v-model="password" class="item" type="text" placeholder="Please enter you pas." show-password />
+        </div>
+        <div >
+          <el-button type="primary" class="button-box">Login</el-button>
+          <div v-if="this.istrue"
+          style="
+          margin: 5px;"
+          class="router-to"
+          >
+            <router-link to="/Register" style="text-decoration: none ;color:black">没有账号，现在去注册</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      istrue: false,
+      value: '',
+      options: [{
+        value: 1,
+        label: '教师'
+      }, {
+        value: 2,
+        label: '学校'
+      }, {
+        value: 3,
+        label: '招办'
+      }]
+    }
+  },
+  methods: {
+    changeselect (selectValue) {
+      console.log(selectValue)
+      if (selectValue === 1) { this.istrue = true } else { this.istrue = false }
+      console.log(this.istrue)
+    }
+  }
+}
+</script>
+
+<style  scoped>
+.line{
+
+}
+i{
+  margin-left: 10px;
+}
+.router-to{
+  color: white;
+  margin-top: 15px;
+  font-size: 13px;
+  height: 40px;
+  text-align: center;
+}
+i{
+  font-size: 20px;
+  /* margin-left:30px ; */
+}
+.box{
+  width: 100vw;
+  height: 100vh;
+  background: url("../assets/back1.jpg") no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: flex;
+}
+
+.login-box{
+  border-radius: 10px;
+  width: 500px;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 15%;
+  background: #cec8c880;
+  padding: 10px 50px;
+  margin: 200px auto auto;
+}
+
+.login-box h1{
+  color: rgba(32, 30, 30, 0.993);
+  margin-top: 0;
+}
+.item{
+  width: 280px; /* 设置合适的宽度 */
+  /* font-size: 18px; 将字体适当的变大加粗 */
+  color: white; /* 上面的文本颜色设置为白色，但是placeholder的颜色要单独设置 */
+  padding: 5px 10px; /* 为了placeholder的内容不是顶格显示，增加内部边界 */
+  margin-left: 10px;
+}
+
+input::-webkit-input-placeholder{
+  color:white;
+}
+.select-box{
+  width: 280px;
+  /* background: #ffffff00; */
+  /* background-color: violet; */
+  padding: 5px 10px;
+  border-radius: 10px;
+  margin-left: 10px;
+}
+.button-box{
+  width: 150px; /* 设置合适的按钮的长和宽 */
+  margin-top: 18px; /* 设置合适的上部外框的宽度，增加与上面的password框的距离 */
+  font-size: 18px;
+  border-radius: 10px;
+  background-image: linear-gradient(to right, #00dbde 0%, #fc00ff 100%); /* 为按钮增加渐变颜色 */
+}
+.regist{
+  color: aqua;
+}
+</style>
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,5 +1,158 @@
 <template>
-  <div class="register">
-    <h1>This is an register page</h1>
+  <div class="box">
+    <!-- <img src="../assets/back-grond.jpg" alt=""> -->
+    <img src="" alt="">
+    <div class="login-box">
+      <div class="login-title"><h1 class="login-title">Regist</h1></div>
+      <div class="form">
+        <div class="line">
+          <i class="el-icon-phone"></i>
+          <el-input v-model="phonenum" class="item" type="text" placeholder="Please enter your phonenum." />
+        </div>
+        <div class="line">
+          <i class="el-icon-key"></i>
+          <el-input v-model="password" class="item" type="text" placeholder="Please enter you pas."  />
+        </div>
+        <div class="line">
+          <i class="el-icon-school"></i>
+          <el-select class="select-box" v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+        <!-- <div>
+          <i class="el-icon-picture-outline"></i>
+          <el-input v-model="identity" class="classifyitem" type="text" placeholder="请输入右中的验证码." />
+          <img src="../assets/classify.jpg" alt="" >
+        </div> -->
+        <div class="line-x">
+
+          <div class="form_font">
+            <i class="el-icon-picture-outline"></i>
+            <el-input v-model="identity" class="classifyitem" type="text" placeholder="请输入右中的验证码." />
+          </div>
+          <img src="../assets/classify.jpg" class="yanzhengma-style"  />
+        </div>
+        <div>
+          <el-button type="primary" class="button-box" >Regist</el-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: '../assets/classify.jpg',
+      phonenum: '',
+      password: '',
+      value: '',
+      identity: '',
+      options: [{
+        value: 1,
+        label: '合工大'
+      }, {
+        value: 2,
+        label: '安徽大学'
+      }, {
+        value: 3,
+        label: '中科大'
+      }]
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style  scoped>
+.line-x{
+  display: flex;
+}
+.yanzhengma-style{
+  width: 90px;
+  height: 40px;
+  border-radius: 10px;
+  margin-top: 5px;
+}
+.form_font {
+  /* line-height: 48px;
+  margin-left: 67px;
+  font-size: 15px; */
+  margin-left:85px ;
+}
+.router-to{
+  color: white;
+  margin-top: 15px;
+  font-size: 13px;
+  height: 40px;
+  text-align: center;
+}
+i{
+  font-size: 20px;
+  /* margin-left:30px ; */
+  /* margin-top: 15px; */
+}
+.box{
+  width: 100vw;
+  height: 100vh;
+  background: url("../assets/back1.jpg") no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: flex;
+}
+
+.login-box{
+  border-radius: 10px;
+  width: 500px;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 15%;
+  background: #cec8c880;
+  padding: 10px 50px;
+  margin: 200px auto auto;
+}
+
+.login-box h1{
+  color: rgba(32, 30, 30, 0.993);
+  margin-top: 0;
+}
+.item{
+  width: 280px; /* 设置合适的宽度 */
+  padding: 5px 10px; /* 为了placeholder的内容不是顶格显示，增加内部边界 */
+  margin-left: 10px;
+}
+.classifyitem{
+  width: 180px;
+  padding: 5px 10px; /* 为了placeholder的内容不是顶格显示，增加内部边界 */
+  margin-left: 10px;
+}
+input::-webkit-input-placeholder{
+  color:white;
+}
+.select-box{
+  width: 280px;
+  /* background: #ffffff00; */
+  /* background-color: violet; */
+  padding: 5px 10px;
+  border-radius: 10px;
+  margin-left: 10px;
+}
+.button-box{
+  width: 150px; /* 设置合适的按钮的长和宽 */
+  margin-top: 18px; /* 设置合适的上部外框的宽度，增加与上面的password框的距离 */
+  font-size: 18px;
+  border-radius: 10px;
+  background-image: linear-gradient(to right, #00dbde 0%, #fc00ff 100%); /* 为按钮增加渐变颜色 */
+}
+.regist{
+  color: aqua;
+}
+</style>
