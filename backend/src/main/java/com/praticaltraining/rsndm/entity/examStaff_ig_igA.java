4d2355c75e76fc6ba709c3b20ca4414d6708a2be
Keyilong <1181448472@qq.com>
2020-12-31 14:17:40 +0800

    添加了两个视图类. Ref #41

---
+++ backend/src/main/java/com/praticaltraining/rsndm/entity/examStaff_ig_igA.java
@@ -0,0 +1,8 @@
+package com.praticaltraining.rsndm.entity;
+
+public class examStaff_ig_igA {
+    public ExamStaff examStaff;
+    public InvigilatorGroup invigilatorGroup;
+    public InvigilatorGroupArrangement invigilatorGroupArrangement;
+
+}
