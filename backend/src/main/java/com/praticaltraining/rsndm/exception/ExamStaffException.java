091a2b47ed44bcc73d6308e8f2ed824c12c57563
Keyilong <1181448472@qq.com>
2021-01-01 15:14:41 +0800

    考务人员注册接口的持久化层+错误处理实现 Ref #55

---
+++ backend/src/main/java/com/praticaltraining/rsndm/exception/ExamStaffException.java
@@ -0,0 +1,5 @@
+package com.praticaltraining.rsndm.exception;
+
+public class ExamStaffException extends BaseException{
+    public ExamStaffException(String message){ super("ExamStaff operation error",message);}
+}
--- backend/src/main/java/com/praticaltraining/rsndm/mapper/TestPersonnelLoginMapper.java
+++ backend/src/main/java/com/praticaltraining/rsndm/mapper/TestPersonnelLoginMapper.java
@@ -1,8 +1,10 @@
 package com.praticaltraining.rsndm.mapper;
 
+import com.praticaltraining.rsndm.entity.TestPersonnelLogin;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TestPersonnelLoginMapper {
     String getPwd(String telephoneNumber);
+    void register(TestPersonnelLogin testPersonnelLogin);
 }
