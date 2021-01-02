5b5e3be8cfa8b2c53ae09a84c16c43fadc2e0b4c
Keyilong <1181448472@qq.com>
2021-01-02 15:26:10 +0800

    考生数目相关接口——持久化层+错误处理实现 Ref #59

---
+++ backend/src/main/java/com/praticaltraining/rsndm/exception/NumberOfCandidatesException.java
@@ -0,0 +1,5 @@
+package com.praticaltraining.rsndm.exception;
+
+public class NumberOfCandidatesException extends BaseException{
+    public NumberOfCandidatesException(String message){ super("NumberOfCandidates operation error",message);}
+}
---
+++ backend/src/main/java/com/praticaltraining/rsndm/mapper/NumberOfCandidatesMapper.java
@@ -0,0 +1,8 @@
+package com.praticaltraining.rsndm.mapper;
+
+import com.praticaltraining.rsndm.entity.NumberOfCandidates;
+
+public interface NumberOfCandidatesMapper {
+    NumberOfCandidates numberOfCandidatesGet();
+    int NumberOfCandidatesUpdate(NumberOfCandidates numberOfCandidates);
+}
