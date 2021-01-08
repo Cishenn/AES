88a34250813dea70049514c14883df0e6f0735dd
Joey_Fan <2225886636@qq.com>
2021-01-08 13:47:13 +0800

    Delete some annotation unused. Ref #135

--- frontend/src/components/teacher/Feedback.vue
+++ frontend/src/components/teacher/Feedback.vue
@@ -1,9 +1,7 @@
-<!--  -->
 <template>
   <div class="box">
     <div class="title-box">提交您的反馈信息</div>
     <div class="form-box">
-      <!-- <div></div> -->
       <el-form label-width="80px">
         <el-form-item label="反馈内容">
           <el-input type="textarea" v-model="reason" style="width:300px"></el-input>
@@ -27,7 +25,6 @@ export default {
   },
   created () {
     if (this.$store.state.teacherId === '') {
-      // alert('不要随便乱进哦!')
       this.$router.push('/login')
     }
   },
@@ -49,7 +46,6 @@ export default {
 }
 .title-box{
   font-size: 20px;
-  /* color: red; */
   margin-bottom: 20px;
 }
 .form-box{
