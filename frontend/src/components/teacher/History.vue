d78785896d57d23b0992e301e242d1825b7c64b7
Joey_Fan <2225886636@qq.com>
2021-01-11 17:07:09 +0800

    Finished frontend realize, waiting backend. Ref #149

--- frontend/src/components/teacher/History.vue
+++ frontend/src/components/teacher/History.vue
@@ -2,13 +2,13 @@
   <div class="box">
     <div class="title-box">历史记录</div>
     <div class="form-box">
-      <el-tabs v-model="activeName" >
+      <el-tabs v-model="activeName"  @tab-click="handleClick">
         <el-tab-pane label="历史监考" name="first">
           <div style="font-size:20px;margin-bottom:10px">历史监考信息表单</div>
-          <el-table border class="table"
-            :data="historyTable.slice((currentPage-1)*pagesize,currentPage*pagesize)"
+          <el-table border class="historyTable"
+            :data="historyTable.slice((hCurrentPage-1)*hPagesize,hCurrentPage*hPagesize)"
             :stripe="true"
-            :current-page.sync="currentPage">
+            :current-page.sync="hCurrentPage">
             <el-table-column label="监考年份" prop="year" align="center" width="220" />
             <el-table-column label="监考详细信息" prop="hsMessage" align="center" width="249" />
           </el-table>
@@ -16,9 +16,9 @@
             <el-pagination
               @size-change="handleSizeChange"
               @current-change="handleCurrentChange"
-              :current-page="currentPage1"
+              :current-page="hCurrentPage"
               :page-sizes="[5,10,15,20]"
-              :page-size="pagesize"
+              :page-size="hPagesize"
               class="pagination"
               layout="total, sizes, prev, pager, next, jumper"
               :total="historyTable.length">
@@ -27,42 +27,26 @@
         </el-tab-pane>
         <el-tab-pane label="历史违规" name="second">
           <div style="font-size:20px;margin-bottom:10px">历史违规信息表单</div>
-          <el-table
-            :data="Violationtable"
-            :stripe="stripe"
-            border
-            style="width: 100%">
-            <el-table-column
-              fixed
-              prop="date"
-              label="违规日期"
-              width="150">
-            </el-table-column>
-            <el-table-column
-              prop="name"
-              label="姓名"
-              width="120">
-            </el-table-column>
-            <el-table-column
-              prop="school"
-              label="考点"
-              width="120">
-            </el-table-column>
-            <el-table-column
-              prop="reason"
-              label="违规原因"
-              width="120">
-            </el-table-column>
-            <el-table-column
-              fixed="right"
-              label="操作"
-              width="100">
-              <template slot-scope="scope">
-                <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-                <el-button type="text" size="small">编辑</el-button>
-              </template>
-            </el-table-column>
+          <el-table border class="violationTable"
+            :data="violationTable.slice((vCurrentPage-1)*vPagesize,vCurrentPage*vPagesize)"
+            :stripe="true"
+            :current-page.sync="vCurrentPage">
+            <el-table-column label="违规年份" prop="year" align="center" width="150" />
+            <el-table-column label="违规详细信息" prop="vrMessage" align="center" width="250" />
+            <el-table-column label="处罚时长" prop="vrPunish" align="center" width="150" />
           </el-table>
+          <div>
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="vCurrentPage"
+              :page-sizes="[5,10,15,20]"
+              :page-size="vPagesize"
+              class="pagination"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="violationTable.length">
+            </el-pagination>
+          </div>
         </el-tab-pane>
         <el-tab-pane label="历史反馈" name="third">历史反馈信息表单</el-tab-pane>
       </el-tabs>
@@ -77,13 +61,15 @@ export default {
       esId: '',
       activeName: 'first',
       historyTable: [],
-      pagesize: 10,
-      currentPage: 1
+      violationTable: [],
+      hPagesize: 10,
+      hCurrentPage: 1,
+      vPagesize: 10,
+      vCurrentPage: 1
     }
   },
   created () {
     if (this.$store.state.teacherId === '') {
-      // alert('不要随便乱进哦!')
       this.$router.push('/login')
     }
     this.esId = this.$store.getters.getTeacherId
@@ -91,6 +77,7 @@ export default {
   },
   mounted () {
     this.getHistoryTable()
+    this.getViolationTable()
   },
 
   methods: {
@@ -105,6 +92,18 @@ export default {
           console.log(resp)
           this.historyTable = resp.data.History
         })
+    },
+    getViolationTable () {
+      // this.$axios
+      //   .get('history/oneHistory', {
+      //     params: {
+      //       esId: this.esId
+      //     }
+      //   })
+      //   .then(resp => {
+      //     console.log(resp)
+      //     this.historyTable = resp.data.History
+      //   })
     }
   }
 }
@@ -128,9 +127,14 @@ export default {
   width: 100%;
 }
 
-.table {
+.historyTable {
   margin-bottom: 2%;
   width: 470px;
+}
+
+.violationTable {
+  margin-bottom: 2%;
+  width: 551px;
 }
 
 .pagination {
