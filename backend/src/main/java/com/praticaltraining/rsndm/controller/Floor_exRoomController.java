9f03c524dbbb388f44b3cd35b37c264da598ee78
Keyilong <1181448472@qq.com>
2021-01-12 15:34:58 +0800

    kyl/getArrgeRoom Ref #165

--- backend/src/main/java/com/praticaltraining/rsndm/controller/Floor_exRoomController.java
+++ backend/src/main/java/com/praticaltraining/rsndm/controller/Floor_exRoomController.java
@@ -68,4 +68,41 @@ public class Floor_exRoomController {
         floor_exRoom floor_exroom = new floor_exRoom(floor,exroom);
         return floor_exroom;
     }
+
+    @GetMapping("/ArrangeFloor_exRoom/schoolId")
+    @ResponseBody
+    @CrossOrigin
+    ResponseEntity<Map<String, List<floor_exRoom>>> queryArrangedBySchoolId(int schoolId){
+        Map<String,List<floor_exRoom>> result = new HashMap<>();
+        List<ExamRoom> room = examRoomBiz.getAllArrangedExamRoom(schoolId);
+        List<Floor> floor = floorBiz.getAllFloor(schoolId);
+        List<floor_exRoom> floor_room = new ArrayList();
+        List<floor_exRoom> fr = new ArrayList();
+        int fId = 0;
+        int flag = 0;
+        for(int i = 0;i<room.size();i++){
+            fId = room.get(i).getFloorId();
+            for(int j = 0;j<floor.size();j++){
+                if(floor.get(j).getFloorId() == fId){
+                    flag = j;
+                    break;
+                }
+            }
+            floor_exRoom floor_exroom = new floor_exRoom(floor.get(flag),room.get(i));
+            floor_room.add(floor_exroom);
+        }
+        List<String> building = floorBiz.getBuilding(schoolId);
+        for(int i = 0;i < building.size();i++){
+            List<floor_exRoom> temp = new ArrayList();
+            for(int j = 0;j < floor_room.size();j++){
+                if(floor_room.get(j).floor.getBuilding().equals(building.get(i))){
+                    temp.add(floor_room.get(j));
+                }
+            }
+            Collections.sort(temp);
+            fr.addAll(temp);
+        }
+        result.put("floor_exRoom",fr);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
