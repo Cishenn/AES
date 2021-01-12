e6e594aa64a0274a98d3ee4dc475a05163130418
Keyilong <1181448472@qq.com>
2021-01-12 12:08:16 +0800

    历史状态备注查询接口 Ref #161

---
+++ backend/src/main/java/com/praticaltraining/rsndm/exception/StatusNotesException.java
@@ -0,0 +1,5 @@
+package com.praticaltraining.rsndm.exception;
+
+public class StatusNotesException extends BaseException{
+    public StatusNotesException(String message){ super("StatusNotes operation error",message);}
+}
